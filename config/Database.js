c94e31ff56ffdacee227596ac213e16e554ada58
faiz effendi <faizeffendi2004@gmail.com>
2025-06-15 15:01:31 +0700

    minor fix

--- config/Database.js
+++ config/Database.js
@@ -7,7 +7,8 @@ dotenv.config();//ngekonfigurasi data dari file.env
 //Bikin variabel yg nerima data yg dirahasiakan
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+// const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PASSWORD = "";
 const DB_HOST = process.env.DB_HOST;
 
 console.log(DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST);
